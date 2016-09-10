fbee611f4bf1c2b892a9750145a8e20cf8ba09fc
vsoch <vsochat@stanford.edu>
2016-09-10 15:42:28 -0700

    modified:   js/web-git.js

--- js/web-git.js
+++ js/web-git.js
@@ -109,22 +109,3 @@ function webGit(url,specfile) {
     this.load_spec();
 
 }
-
-/* To set and get different data objects
-var o = {
-  a: 7,
-  get b() { 
-    return this.a + 1;
-  },
-  set c(x) {
-    this.a = x / 2
-  }
-};
-
-console.log(o.a); // 7
-console.log(o.b); // 8
-o.c = 50;
-console.log(o.a); // 25
-*/
-
-
